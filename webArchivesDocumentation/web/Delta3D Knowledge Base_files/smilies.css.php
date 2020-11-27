6c401433d71065ae790e181f0273e4c14b66e29f
Sparsh Priyadarshi <sparsh11194@gmail.com>
2020-11-27 03:15:49 -0500

    adding a page

---
+++ webArchivesDocumentation/web/Delta3D Knowledge Base_files/smilies.css.php
@@ -0,0 +1,108 @@
+div#forum_smilies a {
+	float: left;
+	padding: 0;
+	margin: 3px;
+}
+.frm_sml {
+	border: 0;
+    width: 16px;
+	height: 16px;
+	background: transparent url('https://web.archive.org/web/20151121193745im_/http://delta3d.org/forum/forum/image_set/smilies.png') no-repeat scroll left top;
+}
+.frm_sml_biggrin {
+    background-position: 0 -208px;
+}
+.frm_sml_smile {
+    background-position: 0 -192px;
+}
+.frm_sml_frown {
+    background-position: 0 -112px;
+}
+.frm_sml_eek {
+    background-position: 0 -272px;
+}
+.frm_sml_confused {
+    background-position: 0 -224px;
+}
+.frm_sml_cool {
+    background-position: 0 -48px;
+}
+.frm_sml_lol {
+    background-position: 0 -352px;
+}
+.frm_sml_angry {
+    background-position: 0 -384px;
+}
+.frm_sml_razz {
+    background-position: 0 -320px;
+}
+.frm_sml_oops {
+    background-position: 0 -144px;
+}
+.frm_sml_surprise {
+    background-position: 0 -176px;
+}
+.frm_sml_cry {
+    background-position: 0 -288px;
+}
+.frm_sml_evil {
+    background-position: 0 -368px;
+}
+.frm_sml_twisted {
+    background-position: 0 -400px;
+}
+.frm_sml_rolleye {
+    background-position: 0 -240px;
+}
+.frm_sml_wink {
+    background-position: 0 -160px;
+}
+.frm_sml_exclaim {
+    background-position: 0 -64px;
+}
+.frm_sml_question {
+    background-position: 0 -96px;
+}
+.frm_sml_idea {
+    background-position: 0 -256px;
+}
+.frm_sml_arrow {
+    background-position: 0 -80px;
+}
+.frm_sml_neutral {
+    background-position: 0 -128px;
+}
+.frm_sml_green {
+    background-position: 0 -0px;
+}
+.frm_sml_sick {
+    background-position: 0 -16px;
+}
+.frm_sml_tired {
+    background-position: 0 -304px;
+}
+.frm_sml_monkey {
+    background-position: 0 -32px;
+}
+
+/*
+     FILE ARCHIVED ON 19:37:45 Nov 21, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:57:27 Nov 27, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  CDXLines.iter: 24.506 (3)
+  PetaboxLoader3.resolve: 25.418
+  PetaboxLoader3.datanode: 98.749 (4)
+  RedisCDXSource: 6.066
+  captures_list: 127.264
+  esindex: 0.014
+  exclusion.robots: 0.292
+  exclusion.robots.policy: 0.278
+  load_resource: 39.804
+  LoadShardBlock: 90.959 (3)
+*/
